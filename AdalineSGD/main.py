dba103dbe14ac1a7de5376f9ee68fde3c69f6df1
Pawel <pawel.a.galas@gmail.com>
2024-10-19 16:41:34 +0200

    added adaline descent gradient and stochastic

--- AdalineSGD/main.py
+++ AdalineSGD/main.py
@@ -1,9 +1,10 @@
+from adalineSGD import AdalineSGD
 import numpy as np
 import os
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
-from adaline import AdalineGD
+
 
 def plot_decision_regions(X, y, classifier, resolution=0.02):
 
@@ -42,19 +43,34 @@ y = np.where(y == 'Iris-setosa', -1, 1)
 # Create train set, take only first and 3rd property
 X = df.iloc[0:100, [0, 2]].values
 
+X_std = np.copy(X)
+X_std[:, 0] = (X[:, 0] - X[:, 0].mean()) / X[:, 0].std()
+X_std[:, 1] = (X[:, 1] - X[:, 1].mean()) / X[:, 1].std()
+
 fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(10, 4))
 
-ada1 = AdalineGD(epochs=10, eta=0.01).fit(X, y)
-ax[0].plot(range(1, len(ada1.cost_) + 1), np.log10(ada1.cost_), marker='o')
-ax[0].set_xlabel('Epochs')
-ax[0].set_ylabel('log(Sum-squared-error)')
-ax[0].set_title('Adaline - Learning rate 0.01')
+ada_sgd = AdalineSGD(epochs=15, eta=0.01, random_state=1)
+ada_sgd.fit(X_std, y)
 
-ada2 = AdalineGD(epochs=10, eta=0.0001).fit(X, y)
-ax[1].plot(range(1, len(ada2.cost_) + 1), ada2.cost_, marker='o')
-ax[1].set_xlabel('Epochs')
-ax[1].set_ylabel('Sum-squared-error')
-ax[1].set_title('Adaline - Learning rate 0.0001')
+plot_decision_regions(X_std, y, classifier=ada_sgd)
+plt.title('Adaline - Stochastic Gradient Descent')
+plt.xlabel('sepal length [standardized]')
+plt.ylabel('petal length [standardized]')
+plt.legend(loc='upper left')
 
-plt.savefig('images/02_11.png', dpi=300)
+plt.tight_layout()
+# plt.savefig('images/02_15_1.png', dpi=300)
 plt.show()
+
+plt.plot(range(1, len(ada_sgd.cost_) + 1), ada_sgd.cost_, marker='o')
+plt.xlabel('Epochs')
+plt.ylabel('Average Cost')
+
+plt.tight_layout()
+# plt.savefig('images/02_15_2.png', dpi=300)
+plt.show()
+
+
+
+
+ada_sgd.partial_fit(X_std[0, :], y[0])
